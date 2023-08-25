0405558fc5a514cdf8cf002573705e6f471fc6be
ahusicic1 <ahusicic1@etf.unsa.ba>
2023-08-25 18:47:17 +0200

    conditional badge rendering

--- components/Card.jsx
+++ components/Card.jsx
@@ -2,10 +2,17 @@ import React from "react"
 
 
 export default function Card(props) {
+    let badgeText
+    if(props.openSpots === 0){
+        badgeText = "SOLD OUT"
+    }else if(props.location === "Online"){
+        badgeText = "ONLINE"
+    }
+
     return (
         <div className="card--div">
-        { props.openSpots === 0 && <div className="card--badge">
-            SOLD OUT
+        { badgeText && <div className="card--badge">
+            {badgeText}
         </div>}
         <img src={`./images/${props.img}`} className="card--image"></img>
      
